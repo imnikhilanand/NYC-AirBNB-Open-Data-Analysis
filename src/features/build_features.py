d3eab227ed4f651434384b43a92f67e2afaebba4
imnikhilanand <imnikhilanand@gmail.com>
2022-03-11 21:44:39 -0500

    Adding src files

---
+++ src/features/build_features.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 11 21:42:35 2022
+
+@author: Nikhil
+"""
+
---
+++ src/models/train_model.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 11 21:43:38 2022
+
+@author: Nikhil
+"""
+
